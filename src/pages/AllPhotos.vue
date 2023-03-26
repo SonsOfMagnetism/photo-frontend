ac9f5497a52f644c3ad3a046854a69107835ea88
Rene Santiago <rsj510@gmail.com>
2023-03-26 16:59:35 -0400

    create pages

---
+++ src/pages/AllPhotos.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>AllPhotos</h1>
+</template>
+
+<script>
+    export default {
+        name: "AllPhotos"
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/SinglePhoto.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Single Photo</h1>
+</template>
+
+<script>
+    export default {
+        name: "SinglePhoto"
+    }
+</script>
+
+<style>
+
+</style>
