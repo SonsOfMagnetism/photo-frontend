fbd8792be348f215c5c6f33a7c3e345bbcd6e194
Rene Santiago <rsj510@gmail.com>
2023-03-26 16:53:56 -0400

    setup routes/main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createWebHistory, createRouter } from 'vue-router'
+import routes from "./routes"
+import 'milligram'
 
-createApp(App).mount('#app')
+// Create router
+const router = createRouter({
+    // Create web history
+    history: createWebHistory(),
+    // Inject routes
+    routes,
+})
+
+// Create vue application
+const app = createApp(App)
+
+// Inject router into app
+app.use(router)
+
+// Mount app to the DOM
+app.mount("#app")
