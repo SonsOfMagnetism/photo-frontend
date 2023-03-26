70d68211f96d99d63a21a8b64d70fab0c355e23f
Rene Santiago <rsj510@gmail.com>
2023-03-26 18:11:20 -0400

    add components to routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,20 @@
 /////////////////////////
 // Components Imports
 /////////////////////////
+import AllPhotos from "./pages/AllPhotos"
+import SinglePhoto from "./pages/SinglePhoto"
+import Form from "./pages/Form"
 
 /////////////////////////
 // Array of Routes
 /////////////////////////
 export default [
-    { path: "/", component: Home },
-    { path: "/about", component: About },
+    // Main page for seeing all photos
+    { path: "/", component: AllPhotos },
+    // Page for seeing an individual photo
+    { path: "/photo/:id", component: SinglePhoto, name: "photo" },
+    // Route for creating a new photo
+    { path: "/new", component: Form },
+    // Route for updating a photo
+    { path:"/edit", component: Form }
 ]
