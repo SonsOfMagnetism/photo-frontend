5e8d44d08b6d082310e8c698ca04caae13d00b01
Rene Santiago <rsj510@gmail.com>
2023-03-28 16:09:10 -0400

    render allphotos

--- src/pages/AllPhotos.vue
+++ src/pages/AllPhotos.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>AllPhotos</h1>
+    <div>
+        <div class="photo">
+            <router-link :to="{name: 'photo'}">
+                <h1></h1>
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +15,10 @@
 </script>
 
 <style>
-
+    .photo {
+        text-align: center;
+        border: 3px solid green;
+        margin: 10px auto;
+        width: 80%;
+    }
 </style>
